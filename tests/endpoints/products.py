2d7888bd998a59212e82eeb6930a5e84267bede3
Nicholas Tulli <ntulli@RS-C02WD0EJHV2W-ntulli.local>
2019-02-11 12:19:10 -0500

    Products, thumbnails, zones endpoints all accounted for

--- tests/endpoints/products.py
+++ tests/endpoints/products.py
@@ -1,6 +1,82 @@
 from tests.base_test import BaseTestClass
+import random
 
 
 class TestProductsEndpoint(BaseTestClass):
-    # TODO: THIS
-    pass
+
+    def test_base_products_with_limit_endpoint(self):
+        params = {'limit': 50}
+        test_against = self.plain_request('products', params=params)
+
+        content = self.rossby.products.get(limit=50)
+
+        self.assertDictEqual(test_against, content.json)
+
+    def test_products_locations_endpoint(self):
+        params = {}
+        test_against = self.plain_request('products/locations', params=params)
+
+        content = self.rossby.products.locations()
+
+        self.assertDictEqual(test_against, content.json)
+
+    def test_product_types_endpoint(self):
+        params = {}
+        test_against = self.plain_request('products/types', params=params)
+
+        content = self.rossby.products.types()
+
+        self.assertDictEqual(test_against, content.json)
+
+    def test_product_by_id_endpoint(self):
+        params = {'limit': 10}
+        all_products = self.plain_request('products', params=params)
+
+        product_id = all_products.get('@graph')[0].get('id')
+
+        test_against = self.plain_request(f'products/{product_id}', params={})
+
+        content = self.rossby.products.by_id(product_id=product_id)
+
+        self.assertDictEqual(test_against, content.json)
+
+    def test_product_by_type_endpoint(self):
+        params = {}
+        test_against = self.plain_request('products/types/AFD', params=params)
+
+        content = self.rossby.products.by_type(type_id='AFD')
+
+        self.assertDictEqual(test_against, content.json)
+
+    def test_product_type_locations_endpoint(self):
+        params = {}
+        test_against = self.plain_request('products/types/AFD/locations', params=params)
+
+        content = self.rossby.products.type_locations(type_id='AFD')
+
+        self.assertDictEqual(test_against, content.json)
+
+    def test_product_location_types_endpoint(self):
+        params = {}
+        forecast_discussion_locations = self.plain_request('products/types/AFD/locations', params=params)
+
+        location_id = random.sample(forecast_discussion_locations.get('locations').items(), 1)[0][0]
+
+        test_against = self.plain_request(f'products/locations/{location_id}/types', params=params)
+
+        content = self.rossby.products.location_types(location_id=location_id)
+
+        self.assertDictEqual(test_against, content.json)
+
+    def test_products_by_type_and_location_endpoint(self):
+        params = {}
+        type_id = 'AFD'
+        forecast_discussion_locations = self.plain_request(f'products/types/{type_id}/locations', params=params)
+
+        location_id = random.sample(forecast_discussion_locations.get('locations').items(), 1)[0][0]
+
+        test_against = self.plain_request(f'products/types/{type_id}/locations/{location_id}', params=params)
+
+        content = self.rossby.products.by_type_and_location(type_id=type_id, location_id=location_id)
+
+        self.assertDictEqual(test_against, content.json)
--- tests/endpoints/thumbnails.py
+++ tests/endpoints/thumbnails.py
@@ -2,5 +2,14 @@ from tests.base_test import BaseTestClass
 
 
 class TestThumbnailsEndpoint(BaseTestClass):
-    # TODO: THIS
-    pass
+
+    def test_thumbnail_by_area_endpoint(self):
+        params = {}
+        test_against = self.plain_request('thumbnails/satellite/PA', params=params)
+
+        if test_against.get('status') == 500:
+            # TODO: Log/output 500 error
+            pass
+        else:
+            content = self.rossby.thumbnails.get_by_area(area='PA')
+            self.assertDictEqual(test_against, content.json)
