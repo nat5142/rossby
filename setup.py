6214953826198bc3c64496ee02d3a3f71d50db31
nat5142 <nick.tulli95@gmail.com>
2019-02-15 20:54:51 -0500

    Had to fix one more thing

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/nat5142/rossby',
-    python_requires='-=3.6',
+    python_requires='>=3.6',
     packages=setuptools.find_packages(exclude='tests'),
-    setup_requires=['requests>=2.20'],
+    setup_requires=['requests>=2.20', 'addict==2.2.0'],
     keywords='national weather service nws noaa api wrapper',
     classifiers=[
         'Programming Language :: Python :: 3',
