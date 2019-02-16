e31784b80d8b65853a018484772513611bac3fab
nat5142 <nick.tulli95@gmail.com>
2019-02-15 20:36:18 -0500

    WIP: Moved package structure to load properly with setuptools

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ with open('README.md', 'r') as readme:
     long_description = readme.read()
 
 setuptools.setup(
-    name='Rossby',
+    name='rossby',
     version='0.1',
     author='Nicholas Tulli',
     author_email='nat5142@psu.edu',
     description='A wrapper for the U.S. National Weather Service API',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='',
-    python_requires='-=3.7',
+    url='https://github.com/nat5142/rossby',
+    python_requires='-=3.6',
     setup_requires=['requests>=2.20'],
     keywords='national weather service nws noaa api wrapper',
     classifiers=[
         'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS :: Independent'
     ]
 )
